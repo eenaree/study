45ed2a06034ece2692b5c9f35624eb1322c1af00
Nari Lee <narilee91@gmail.com>
2022-11-17 22:30:15 +0900

    Chore: add compiled script file

---
+++ project/calculator/index.js
@@ -0,0 +1,184 @@
+"use strict";
+const clearBtn = document.querySelector('#clear');
+const clearEntryBtn = document.querySelector('#clear_entry');
+const dotBtn = document.querySelector('#dot');
+const equalBtn = document.querySelector('#equal');
+const numberButtons = document.querySelectorAll('.number');
+const operatorButtons = document.querySelectorAll('.operator');
+const expression = document.querySelector('.expression');
+const result = document.querySelector('.result');
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
+let isFirst = false;
+let equal = false;
+if (!(result instanceof HTMLElement)) {
+    throw new Error('result must be an HTML element');
+}
+if (!(expression instanceof HTMLElement)) {
+    throw new Error('expression must be be an HTML element');
+}
+const calculate = (operator, firstNumber, secondNumber) => {
+    let output = 0;
+    switch (operator) {
+        case '+':
+            output = firstNumber + secondNumber;
+            break;
+        case '-':
+            output = firstNumber - secondNumber;
+            break;
+        case '*':
+            output = firstNumber * secondNumber;
+            break;
+        case '÷':
+            output = firstNumber / secondNumber;
+            break;
+        default:
+            console.log('matching operator is not exist.');
+    }
+    return output;
+};
+for (const numberButton of numberButtons) {
+    numberButton.addEventListener('click', () => {
+        // 현재 숫자가 0인 경우
+        // 1. 0 클릭 => 무시
+        // 2. 0 이외 클릭 => 그 값으로 변경
+        // 현재 숫자가 0이 아닌 경우
+        // 숫자를 클릭할 때마다 그 숫자를 이전 숫자의 오른쪽에 추가
+        if (numberButton.textContent === null)
+            return;
+        if (equal) {
+            expression.textContent = '';
+            result.textContent = numberButton.textContent;
+            firstNumber = Number(result.textContent);
+            equal = false;
+            return;
+        }
+        if (isFirst) {
+            result.textContent = numberButton.textContent;
+            secondNumber = Number(result.textContent);
+            isFirst = false;
+            return;
+        }
+        if (result.textContent === '0') {
+            result.textContent = numberButton.textContent;
+        }
+        else {
+            result.textContent += numberButton.textContent;
+        }
+        if (operator === '') {
+            firstNumber = Number(result.textContent);
+        }
+        else {
+            secondNumber = Number(result.textContent);
+        }
+    });
+}
+for (const operatorButton of operatorButtons) {
+    operatorButton.addEventListener('click', () => {
+        // 연산자가 없는 경우 (첫번째 숫자 입력 단계)
+        // => 연산자를 클릭할 경우, 연산을 실행하지 않고 연산자를 추가함.
+        // => 현재 숫자를 이전 숫자에 저장하고, 표현식을 변경함.
+        // 연산자가 이미 있는 경우 (두번째 숫자 입력 단계)
+        // 1. 기존 연산자와 다른 연산자를 클릭한 경우
+        // 숫자를 한번이라도 입력했다면 => 이전 연산자로 연산 실행 후, 연산자 변경
+        // 숫자를 한번도 입력하지 않았다면 => 연산자 변경만 함
+        // 2. 기존 연산자와 같은 연산자를 클릭한 경우
+        // 숫자를 한번이라도 입력했다면 => 연산 실행
+        // 숫자를 한번도 입력하지 않았다면면 => 연산 실행 x
+        if (operatorButton.textContent === null)
+            return;
+        if (equal) {
+            operator = operatorButton.textContent;
+            firstNumber = Number(result.textContent);
+            secondNumber = Number(result.textContent);
+            expression.textContent = `${firstNumber} ${operator}`;
+            equal = false;
+            isFirst = true;
+            return;
+        }
+        if (isFirst) {
+            operator = operatorButton.textContent;
+            expression.textContent = `${firstNumber} ${operator}`;
+            result.textContent = `${firstNumber}`;
+            secondNumber = firstNumber;
+            return;
+        }
+        if (operator === '') {
+            operator = operatorButton.textContent;
+            expression.textContent = `${firstNumber} ${operator}`;
+            result.textContent = `${firstNumber}`;
+            secondNumber = firstNumber;
+            isFirst = true;
+            return;
+        }
+        if (operator !== operatorButton.textContent) {
+            const output = calculate(operator, firstNumber, secondNumber);
+            operator = operatorButton.textContent;
+            firstNumber = output;
+            secondNumber = output;
+            expression.textContent = `${firstNumber} ${operator}`;
+            result.textContent = `${output}`;
+            isFirst = true;
+            return;
+        }
+        const output = calculate(operator, firstNumber, secondNumber);
+        firstNumber = output;
+        secondNumber = output;
+        expression.textContent = `${firstNumber} ${operator}`;
+        result.textContent = `${output}`;
+        isFirst = true;
+    });
+}
+clearBtn?.addEventListener('click', () => {
+    // 초기화
+    expression.textContent = '';
+    result.textContent = '0';
+    firstNumber = 0;
+    secondNumber = 0;
+    operator = '';
+});
+clearEntryBtn?.addEventListener('click', () => {
+    // 현재 입력 숫자 삭제
+    // 수식 계산이 이뤄진 후에 클릭한 경우 => 전부 초기화
+    if (equal) {
+        expression.textContent = '';
+        result.textContent = '0';
+        firstNumber = 0;
+        secondNumber = 0;
+        operator = '';
+    }
+    else {
+        result.textContent = '0';
+        isFirst = true;
+    }
+});
+dotBtn?.addEventListener('click', () => {
+    // 현재 입력 숫자에 점이 포함되어 있다면, 점 입력 더이상 불가
+    // 두번째 숫자를 한번도 입력하지 않은 경우 => 0. 형태로 추가
+    // 한번이라도 입력한 경우 => 현재 숫자에 .만 붙임
+    if (result.textContent?.includes('.'))
+        return;
+    if (isFirst) {
+        result.textContent = '0.';
+        isFirst = false;
+    }
+    else {
+        result.textContent += '.';
+    }
+});
+equalBtn?.addEventListener('click', () => {
+    // 연산자가 존재하지 않는 경우 => 무시
+    // 연산자가 존재하는 경우 => 연산 실행
+    // 연산이 실행됐다면 => 표현식에 숫자들과 연산자, equal 까지 표현
+    // 연산이 실행된 후,
+    // 1. 숫자 클릭 => 클릭한 숫자만 현재 숫자로 보여주기(결과창), 그 이외 값은 초기화
+    // 2. 연산자 클릭 => 클릭한 연산자와 현재 결과를 합친 표현식 보여주기. 첫번째 입력 상태 여부 초기화
+    if (operator === '')
+        return;
+    const output = calculate(operator, firstNumber, secondNumber);
+    expression.textContent = `${firstNumber} ${operator} ${secondNumber} =`;
+    result.textContent = `${output}`;
+    operator = '';
+    equal = true;
+});
---
+++ project/simpleNote/index.js
@@ -0,0 +1,236 @@
+"use strict";
+const $addBtn = document.querySelector('#add_btn');
+const $noteList = document.querySelector('#note_list');
+const $rightPanel = document.querySelector('#right');
+const $noteTitle = $rightPanel?.querySelector('#note_title');
+const $noteBody = $rightPanel?.querySelector('#note_body');
+const $modal = document.getElementById('modal');
+const $deleteBtn = $modal?.querySelector('.delete');
+const $deleteCancelBtn = $modal?.querySelector('.cancel');
+const $allClearBtn = document.querySelector('#all_clear_btn');
+if (!($addBtn instanceof HTMLButtonElement)) {
+    throw new Error('addBtn must be HTML Button Element');
+}
+if (!($noteList instanceof HTMLDivElement)) {
+    throw new Error('noteList must be HTML Div Element');
+}
+if (!($rightPanel instanceof HTMLDivElement)) {
+    throw new Error('rightPanel must be HTML Div Element');
+}
+if (!($noteTitle instanceof HTMLInputElement)) {
+    throw new Error('noteTitle must be HTML Input Element');
+}
+if (!($noteBody instanceof HTMLTextAreaElement)) {
+    throw new Error('noteBody must be HTML TextArea Element');
+}
+if (!($modal instanceof HTMLDivElement)) {
+    throw new Error('modal must be HTML Div Element');
+}
+if (!($allClearBtn instanceof HTMLButtonElement)) {
+    throw new Error('allClearBtn must be HTML Button Element');
+}
+const getStorageNotes = () => {
+    const storageNotes = localStorage.getItem('notes');
+    if (!storageNotes) {
+        localStorage.setItem('notes', JSON.stringify([]));
+        return [];
+    }
+    const parsedNotes = JSON.parse(storageNotes);
+    if (isNotes(parsedNotes)) {
+        return parsedNotes;
+    }
+    localStorage.setItem('notes', JSON.stringify([]));
+    return [];
+};
+const isNotes = (values) => {
+    if (Array.isArray(values)) {
+        return values.every(value => 'id' in value && 'title' in value && 'body' in value && 'date' in value);
+    }
+    return false;
+};
+const notes = getStorageNotes();
+let currentNote = notes.length > 0 ? notes[notes.length - 1] : null;
+const createNote = (note) => {
+    const $div = document.createElement('div');
+    for (const $note of $noteList.children) {
+        $note.classList.remove('active');
+    }
+    $div.classList.add('active');
+    $div.dataset.id = `${note.id}`;
+    for (let i = 0; i < 3; i++) {
+        const $span = document.createElement('span');
+        switch (i) {
+            case 0:
+                $span.textContent = note.title || '제목을 입력하세요';
+                $span.classList.add('title');
+                break;
+            case 1:
+                $span.textContent = note.body || '내용을 입력하세요';
+                $span.classList.add('cont');
+                break;
+            case 2:
+                $span.textContent = note.date;
+                $span.classList.add('date');
+                break;
+        }
+        $div.append($span);
+    }
+    const $button = document.createElement('button');
+    $button.textContent = '삭제';
+    $button.addEventListener('click', openModal);
+    $div.append($button);
+    $noteList.prepend($div);
+};
+const getTimestamp = () => new Date().toLocaleString();
+const addNote = () => {
+    const newNote = {
+        id: notes.length > 0 ? notes[notes.length - 1].id + 1 : 1,
+        title: '',
+        body: '',
+        date: getTimestamp(),
+    };
+    currentNote = newNote;
+    notes.push(newNote);
+    localStorage.setItem('notes', JSON.stringify(notes));
+    createNote(newNote);
+    renderCurrentNote();
+};
+const renderNoteList = () => {
+    notes.forEach(note => {
+        const $div = document.createElement('div');
+        $div.classList.toggle('active', note.id === currentNote?.id);
+        $div.dataset.id = `${note.id}`;
+        for (let i = 0; i < 3; i++) {
+            const $span = document.createElement('span');
+            switch (i) {
+                case 0:
+                    $span.textContent = note.title || '제목을 입력하세요';
+                    $span.classList.add('title');
+                    break;
+                case 1:
+                    $span.textContent = note.body || '내용을 입력하세요';
+                    $span.classList.add('cont');
+                    break;
+                case 2:
+                    $span.textContent = note.date;
+                    $span.classList.add('date');
+                    break;
+            }
+            $div.append($span);
+        }
+        const $button = document.createElement('button');
+        $button.textContent = '삭제';
+        $button.addEventListener('click', openModal);
+        $div.append($button);
+        $noteList.prepend($div);
+    });
+};
+const renderCurrentNote = () => {
+    if (currentNote) {
+        $noteTitle.value = currentNote.title;
+        $noteBody.value = currentNote.body;
+        $rightPanel.hidden = false;
+    }
+    else {
+        $noteTitle.value = '';
+        $noteBody.value = '';
+        $rightPanel.hidden = true;
+    }
+};
+const openNote = (e) => {
+    if (e.target instanceof HTMLElement) {
+        const $realTarget = e.target.closest('div');
+        if (!($realTarget instanceof HTMLElement))
+            return;
+        const matchedNote = notes.find(value => value.id === Number($realTarget.dataset.id));
+        if (!matchedNote)
+            return;
+        currentNote = matchedNote;
+        for (const $note of $noteList.children) {
+            if ($note instanceof HTMLElement) {
+                $note.classList.toggle('active', Number($note.dataset.id) === currentNote.id);
+            }
+        }
+        renderCurrentNote();
+    }
+};
+const editNote = (e) => {
+    if (!currentNote)
+        return;
+    if (e.target instanceof HTMLInputElement) {
+        currentNote.title = e.target.value;
+    }
+    if (e.target instanceof HTMLTextAreaElement) {
+        currentNote.body = e.target.value;
+    }
+};
+const updateNote = () => {
+    if (!currentNote)
+        return;
+    notes.forEach(note => {
+        if (note.id === currentNote?.id) {
+            note.title = currentNote.title;
+            note.body = currentNote.body;
+            note.date = getTimestamp();
+        }
+    });
+    localStorage.setItem('notes', JSON.stringify(notes));
+    for (const $note of $noteList.children) {
+        const title = $note.querySelector('.title');
+        const cont = $note.querySelector('.cont');
+        const date = $note.querySelector('.date');
+        if (!(title && cont && date))
+            return;
+        if ($note instanceof HTMLElement) {
+            if (Number($note.dataset.id) !== currentNote.id)
+                continue;
+            title.textContent = currentNote?.title || '제목을 입력하세요';
+            cont.textContent = currentNote?.body || '내용을 입력하세요';
+            date.textContent = currentNote?.date;
+        }
+    }
+};
+const openModal = () => {
+    $modal.hidden = false;
+};
+const closeModal = () => {
+    $modal.hidden = true;
+};
+const deleteNote = (e) => {
+    if (e.target instanceof HTMLElement) {
+        const matchedNoteIndex = notes.findIndex(note => note.id === currentNote?.id);
+        if (matchedNoteIndex === -1)
+            return;
+        for (const $note of $noteList.children) {
+            if ($note instanceof HTMLElement) {
+                if (Number($note.dataset.id) !== currentNote?.id)
+                    continue;
+                $note.remove();
+                $noteList.firstElementChild?.classList.add('active');
+                break;
+            }
+        }
+        notes.splice(matchedNoteIndex, 1);
+        localStorage.setItem('notes', JSON.stringify(notes));
+        currentNote = notes.length > 0 ? notes[notes.length - 1] : null;
+        renderCurrentNote();
+        closeModal();
+    }
+};
+const deleteNotes = () => {
+    notes.splice(0);
+    currentNote = null;
+    $noteList.innerHTML = '';
+    renderCurrentNote();
+};
+renderNoteList();
+renderCurrentNote();
+$addBtn.addEventListener('click', addNote);
+$noteList.addEventListener('click', openNote);
+$noteTitle.addEventListener('input', editNote);
+$noteBody.addEventListener('input', editNote);
+$noteTitle.addEventListener('change', updateNote);
+$noteBody.addEventListener('change', updateNote);
+$deleteBtn?.addEventListener('click', deleteNote);
+$deleteCancelBtn?.addEventListener('click', closeModal);
+$allClearBtn.addEventListener('click', deleteNotes);
